85b634490b7578add9ba04ee3a38009a3da9cd77
Ngi Bujri <nbujri7@gmail.com>
2023-03-26 20:32:09 -0400

    add screenshot for 2.3

--- week_2/bujri_myworld.py
+++ week_2/bujri_myworld.py
@@ -1,1 +1,9 @@
+"""
+  title: bujri_myworld.py
+  author: ngi bujri
+  date: march 20 2023
+  description: introductory app to print a message
+"""
+
+# print message to terminal
 print("You are now in Bujri's world!")
